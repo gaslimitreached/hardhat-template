4e1d91b73f36691f070851847bcb86b553997cda
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2021-12-15 19:22:15 -0600

    chore: convert deploy script to typescript

---
+++ deploy/00_deploy_propbaly_nothing.ts
@@ -0,0 +1,18 @@
+// deploy/00_deploy_probably_nothing.ts
+import * as ethers from 'ethers'
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await deploy('ProbablyNothing', {
+    from: deployer,
+    args: [
+      2, // max supply
+      ethers.utils.parseEther('0.04') // price 0.04 ether
+    ],
+    log: true
+  })
+}
+
+module.exports.tags = ['ProbablyNothing']
