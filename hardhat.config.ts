e29b9de3a0e30cecb59fc25c6ae9e1879defdb37
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2022-03-05 12:03:14 -0600

    chore: support dot env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
@@ -8,24 +10,37 @@ import 'solidity-coverage';
 
 import { HardhatUserConfig } from 'hardhat/config';
 
+// default values are there to avoid failures when running tests
+const RINKEBY_RPC = process.env.RIKENBY_RPC || '1'.repeat(32);
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '1'.repeat(64);
+
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: { chainId: 1337 },
+    rinkeby: {
+      url: RINKEBY_RPC,
+      accounts: [PRIVATE_KEY],
+    }
   },
   solidity: {
-    version: '0.8.9',
+    version: '0.8.10',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1,
+        runs: 200,
       },
     },
   },
   gasReporter: {
     currency: 'USD',
     gasPrice: 77,
-    coinmarketcap: process.env.CMC_KEY
+    // API key for CoinMarketCap. https://pro.coinmarketcap.com/signup
+    coinmarketcap: process.env.CMC_KEY ?? '',
+  },
+  etherscan: {
+    // API key for Etherscan. https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY ?? '',
   },
   namedAccounts: {
     deployer: {
